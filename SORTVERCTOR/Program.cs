b98d9e9700a5c0cbff456febe6dc1e975558b351
Endi Dyrmyshi <ENDI.DYRMYSHI@local.ittcesena.it>
2020-10-07 11:06:42 +0200

    First Step

---
+++ SORTVERCTOR/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SORTVERCTOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vett = new int[10] { -4,1,100,-85,4,6,77,-35,2,0};
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write(vett[i] + "/");
+
+                
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
